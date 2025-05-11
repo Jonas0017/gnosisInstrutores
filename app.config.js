e6fea557f05e4e0a37755a8da2e2dc9bf09c3f1b
jonas0017 <jonasavila@hotmail.com>
2025-05-11 17:52:18 -0300

    limpeza: removido expo-build-properties e config quebrada

--- app.config.js
+++ app.config.js
@@ -11,55 +11,42 @@ export default {
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     android: {
       package: "com.gnosisbrasil.instrutores",
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ecf0f1"
-      }
+        backgroundColor: "#ffffff",
+      },
     },
     web: {
       bundler: "metro",
       output: "static",
-      favicon: "./assets/images/favicon.png"
+      favicon: "./assets/images/favicon.png",
     },
     plugins: [
       "expo-router",
-      [
-        "expo-build-properties",
-        {
-          android: {
-            kotlinVersion: "1.9.10",
-            gradlePluginVersion: "7.4.1",
-            compileSdkVersion: 34,
-            targetSdkVersion: 34,
-            minSdkVersion: 23
-          }
-        }
-      ],
       [
         "expo-splash-screen",
         {
           image: "./assets/images/splash-icon.png",
           imageWidth: 200,
           resizeMode: "contain",
-          backgroundColor: "#ecf0f1"
-        }
-      ]
+          backgroundColor: "#ffffff",
+        },
+      ],
     ],
     experiments: {
-      typedRoutes: true
+      typedRoutes: true,
     },
     extra: {
       router: {
         origin: false,
-        output: ".expo"
       },
       eas: {
-        projectId: "6dc0832b-8654-4d0d-98a4-1b7528c58b1b"
-      }
-    }
-  }
+        projectId: "6dc0832b-8654-4d0d-98a4-1b7528c58b1b",
+      },
+    },
+  },
 };
